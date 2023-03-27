b0a912a87d67df68c0d43879b4b54cfdd4321bf7
Raj Prajapati <prajapatiraj8212@gmail.com>
2023-03-27 21:36:37 +0530

    quiz

---
+++ src/Data/QuizData.js
@@ -0,0 +1,27 @@
+export const QuizData = [
+{
+    question: "Which of the following is the correct name of React.js?",
+    options:["React",
+        "React.js",
+        "ReactJS",
+        "All of the above"],
+    answer:4
+},
+{
+    question: "What is your city name?",
+    options:["Banaras","Jaunpur","Allahabad","Lucknow"],
+    answer:2
+},
+{
+    question: "What is your age?",
+    options:["13","22","21","30"],
+    answer:2
+},
+{
+    question: "What is your PC name ?",
+    options:["HP","Lenovo","Dell","Acer"],
+    answer:4
+},
+
+
+];
